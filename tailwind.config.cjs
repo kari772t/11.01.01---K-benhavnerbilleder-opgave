f3f9101a6440a6dadc8a1f4186764e1bc2870e7e
”karina <kari772t@stud.kea.dk>
2022-08-26 11:12:26 +0200

    farver

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgr: "#D4DFE2",
+        hero: "#333940",
+        button: "#43a1fe",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
